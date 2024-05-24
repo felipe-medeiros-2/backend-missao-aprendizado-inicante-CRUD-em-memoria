2152343a5e791290bed94cdfbec5ebff75b70b7f
felipe-medeiros-2 <169067425+felipe-medeiros-2@users.noreply.github.com>
2024-05-24 02:35:07 +0000

    Endpoint Read By ID '[GET]/personagem/:id' criado

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ app.get('/', function (req, res) {
 
 const lista = ['Java','Kotlin','Android']
 
-// Endpoint Read All [GET]/personagem 
+// Endpoint Read All [GET] / personagem/
 
 app.get('/personagem', function (req, res) {
     res.send(lista)
   })
 
 
+// Endpoint Read By ID [GET]/personagem/:id
+
+app.get('/personagem/:id', function (req, res) {
+    // Acessamos o o parÂmetro de rota ID
+    const id = req.params.id
+    // Acessa o item na lista usando o ID - 1
+    const item = lista[id]
+    res.send(item)
+  })
 app.listen(3000)
